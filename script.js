b149fbad2582ca014eb8862970cd460df0e87ab7
joaoraulino <joaovictorraulino1@gmail.com>
2023-06-27 13:18:27 -0300

    Removendo arquivos antigos

--- script.js
+++
@@ -1,6 +0,0 @@
-var file = document.getElementById("inImg");
-var img = document.getElementById("image");
-
-file.addEventListener("change", (e) =>{
-  img.src = URL.createObjectURL(e.target.files[0])
-});
